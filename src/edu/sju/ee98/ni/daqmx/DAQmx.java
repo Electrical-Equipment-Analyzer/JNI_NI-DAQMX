2e604ff26e9841641ae164aee8371dbaaa1cd658
Leo <Leo@LeoHsueh-NB>
2013-05-27 10:18:09 +0800

    Test jni callback.

--- src/edu/sju/ee98/ni/daqmx/DAQmx.java
+++ src/edu/sju/ee98/ni/daqmx/DAQmx.java
@@ -18,12 +18,16 @@ public class DAQmx {
 
     public native double[] acqIntClk();
 
+    private void callback() {
+        System.out.println("In Java");
+    }
+
     public static void main(String[] args) {
         DAQmx daQmx = new DAQmx();
         daQmx.print();
-        double[] acqIntClk = daQmx.acqIntClk();
-        for (int i = 0; i < acqIntClk.length; i++) {
-            System.out.print(acqIntClk[i] + "\t");
-        }
+//        double[] acqIntClk = daQmx.acqIntClk();
+//        for (int i = 0; i < acqIntClk.length; i++) {
+//            System.out.print(acqIntClk[i] + "\t");
+//        }
     }
 }
